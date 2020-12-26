190b525b45bfaac42b1c3b5c1ea73d702ad87d3c
IWATA1 <abc1123s@icloud.com>
2020-12-26 11:08:37 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,62 @@
+class BooksController < ApplicationController
+
+ before_action :authenticate_user!
+ before_action :ensure_correct_user, only: [:update, :edit]
+
+def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @books = Book.new
+end
+
+def edit
+    @book= Book.find(params[:id])
+end
+
+def update
+    @book = Book.find(params[:id])
+    if @book.update(book_paramas)
+    redirect_to book_path(@book), notice: 'You have updated book successfully.'
+    else
+        render :edit
+    end
+end
+
+def create
+    @book = Book.new(book_paramas)
+    @book.user_id = current_user.id
+    if @book.save
+    redirect_to book_path(@book), notice: 'You have created book successfully.'
+    else
+        @books = Book.all
+        @user = current_user
+        render :index
+    end
+end
+
+def  index
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
+end
+
+def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to action: "index"
+end
+
+private
+def book_paramas
+    params.require(:book).permit(:title, :body)
+end
+
+ def ensure_correct_user
+    @book = Book.find_by(id:params[:id])
+    if @book.user_id != current_user.id
+      redirect_to books_path
+    end
+ end
+ 
+
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'f5ca78ed8dd0ded9e14b8842cea49d07cc836bab5b39698264c732f098f053e54f128026fd9d42ba982f6f9e42477aaa9adbf3b27213f2d3ed7f9a005978a215'
